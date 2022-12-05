d4bd541d8a1d263c39b263dfd9e69e9836d8d503
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-05 10:15:20 +0100

    zadanie 1

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #test
+#pandas,numpy, matplotlib/seaborn
+#netmico, paramico
+#Selenium
+#Pycharm, Visual Studio Code
+#print('hello')
+# print('hello')
+# print("hello")
+# print("Lubię Mc'Donalds")
+# owoc='banan'
+# kolor='żółty'
+# print(owoc)
+# print('Mój ulubiony owoc to '+owoc+". Jego kolor "+kolor)
+# print('Mój ulubiony owoc to {}. Jego kolor {}'.format(owoc,kolor))
+# print(f'Mój ulubiony owoc to {owoc}. Jego kolor {kolor}')
+#String owoc='banan' #java
+#
+# owoc=input('Podaj swój ulubiony owoc:\n')
+# kolor=input('Podaj kolor swojego ulubionego owocu:\n')
+# print('Mój ulubiony owoc to '+owoc+". Jego kolor "+kolor)
+# print('Mój ulubiony owoc to {}. Jego kolor {}'.format(owoc,kolor))
+# print(f'Mój ulubiony owoc to {owoc}. Jego kolor {kolor}')
+#
+
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+# wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
